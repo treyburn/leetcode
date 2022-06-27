13d2a664ca6e00763c7d870e4525ec274f69cd98
treyburn <travisreyburn@gmail.com>
2022-06-27 16:51:40 -0600

    narray tree preoder solution for both revursive and iterative

---
+++ tree/preorder.go
@@ -0,0 +1,43 @@
+package tree
+
+/**
+* Definition for a Node.
+* type Node struct {
+*     Val int
+*     Children []*Node
+* }
+ */
+
+type Node struct {
+	Val      int
+	Children []*Node
+}
+
+// recursive solution
+func preorder_recrusive(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+	output := []int{root.Val}
+
+	for _, child := range root.Children {
+		output = append(output, preorder_recrusive(child)...)
+	}
+
+	return output
+}
+
+func preorder(root *Node) []int {
+	if root == nil {
+		return nil
+	}
+	output := []int{root.Val}
+	kiddos := root.Children
+
+	for len(kiddos) > 0 {
+		output = append(output, kiddos[0].Val)
+		kiddos = append(kiddos[0].Children, kiddos[1:]...)
+	}
+
+	return output
+}
---
+++ tree/preorder_test.go
@@ -0,0 +1,82 @@
+package tree
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+var example2 = &Node{
+	Val: 1,
+	Children: []*Node{
+		{Val: 2},
+		{Val: 3, Children: []*Node{
+			{Val: 6},
+			{Val: 7, Children: []*Node{
+				{Val: 11, Children: []*Node{
+					{Val: 14},
+				}},
+			}},
+		}},
+		{Val: 4, Children: []*Node{
+			{Val: 8, Children: []*Node{
+				{Val: 12},
+			}},
+		}},
+		{Val: 5, Children: []*Node{
+			{Val: 9, Children: []*Node{
+				{Val: 13},
+			}},
+			{Val: 10},
+		}},
+	},
+}
+
+func Test_preorder(t *testing.T) {
+	type testCase struct {
+		name  string
+		input *Node
+		want  []int
+	}
+
+	example1 := &Node{
+		Val: 1,
+		Children: []*Node{
+			{
+				Val:      3,
+				Children: []*Node{{Val: 5}, {Val: 6}},
+			},
+			{Val: 2},
+			{Val: 4},
+		},
+	}
+
+	var tests = []testCase{
+		{"example 1", example1, []int{1, 3, 5, 6, 2, 4}},
+		{"example 2", example2, []int{1, 2, 3, 6, 7, 11, 14, 4, 8, 12, 5, 9, 13, 10}},
+		{"nil", nil, nil},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			tc := test
+
+			got := preorder(tc.input)
+
+			assert.Equal(t, tc.want, got)
+		})
+	}
+}
+
+var preoderResult []int
+
+func Benchmark_preorder_recursive(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		preoderResult = preorder_recrusive(example2)
+	}
+}
+
+func Benchmark_preorder_iterative(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		preoderResult = preorder(example2)
+	}
+}
